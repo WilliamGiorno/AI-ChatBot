f8324e27800714f51c6af6eb5b2c7dfbf3a97b18
WilliamGiorno <70899240+WilliamGiorno@users.noreply.github.com>
2023-05-19 22:20:53 +0200

    add docker

--- app.py
+++ app.py
@@ -42,5 +42,4 @@ def get_bot_response():
     return response.choices[0].message["content"]
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(port=int(os.environ.get("PORT", 5000)), host="0.0.0.0", debug=True)
